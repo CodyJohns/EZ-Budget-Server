5e2e2888268518eb4c2f45355573d38378a6072f
Cody Johns <cjohns365@gmail.com>
2025-12-24 20:50:32 -0700

    Add log statements to debug container env

--- src/main/java/app/ezbudget/server/ezbudgetserver/controllers/EntriesController.java
+++ src/main/java/app/ezbudget/server/ezbudgetserver/controllers/EntriesController.java
@@ -36,115 +36,125 @@ public class EntriesController {
 
         try {
             return ResponseEntity.ok(gson.toJson(service.getEntries(authtoken).getData()));
-        } catch(Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 
     @GetMapping("/all/paged/{page}")
-    public ResponseEntity<String> allEntriesPaged(@RequestHeader("Authorization") String authtoken, @PathVariable("page") int page) {
+    public ResponseEntity<String> allEntriesPaged(@RequestHeader("Authorization") String authtoken,
+            @PathVariable("page") int page) {
 
         EntryService service = new EntryService(factory);
 
         try {
             return ResponseEntity.ok(gson.toJson(service.getEntriesPaged(authtoken, page).getData()));
-        } catch(Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 
     @GetMapping("/full/{id}")
-    public ResponseEntity<String> fullEntry(@RequestHeader("Authorization") String authtoken, @PathVariable("id") String id) {
+    public ResponseEntity<String> fullEntry(@RequestHeader("Authorization") String authtoken,
+            @PathVariable("id") String id) {
 
         EntryService service = new EntryService(factory);
 
         try {
             return ResponseEntity.ok(gson.toJson(service.getEntry(authtoken, id).getData()));
-        } catch(NullPointerException e) {
+        } catch (NullPointerException e) {
             return ResponseEntity.status(404).body(e.getMessage());
-        } catch(Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 
     @PostMapping("/add")
-    public ResponseEntity<String> addEntry(@RequestHeader("Authorization") String authtoken, @RequestParam("json") String json) {
+    public ResponseEntity<String> addEntry(@RequestHeader("Authorization") String authtoken,
+            @RequestParam("json") String json) {
 
         Entry entry = gson.fromJson(json, Entry.class);
 
-        if(entry == null)
+        if (entry == null)
             return ResponseEntity.badRequest().build();
 
         EntryService service = new EntryService(factory);
 
         try {
             return ResponseEntity.ok(gson.toJson(service.addEntry(authtoken, entry).getMessage()));
-        } catch(Exception e) {
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 
     @PostMapping("/update")
-    public ResponseEntity<String> updateEntry(@RequestHeader("Authorization") String authtoken, @RequestParam("json") String json) {
+    public ResponseEntity<String> updateEntry(@RequestHeader("Authorization") String authtoken,
+            @RequestParam("json") String json) {
 
         Entry entry = gson.fromJson(json, Entry.class);
 
-        if(entry == null)
+        if (entry == null)
             return ResponseEntity.badRequest().build();
 
         EntryService service = new EntryService(factory);
 
         try {
             return ResponseEntity.ok(gson.toJson(service.updateEntry(authtoken, entry).getMessage()));
-        } catch(Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteEntry(@RequestHeader("Authorization") String authtoken, @PathVariable("id") String id) {
+    public ResponseEntity<String> deleteEntry(@RequestHeader("Authorization") String authtoken,
+            @PathVariable("id") String id) {
 
         EntryService service = new EntryService(factory);
 
         try {
             return ResponseEntity.ok(gson.toJson(service.deleteEntry(authtoken, id).getMessage()));
-        } catch(Exception e) {
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 
     @GetMapping("/marked/{id}")
-    public ResponseEntity<String> isEntryMarked(@RequestHeader("Authorization") String authtoken, @PathVariable("id") String id) {
+    public ResponseEntity<String> isEntryMarked(@RequestHeader("Authorization") String authtoken,
+            @PathVariable("id") String id) {
 
         EntryService service = new EntryService(factory);
 
         try {
             return ResponseEntity.ok(gson.toJson(service.getMarked(authtoken, id).getData()));
-        } catch(Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 
     @PostMapping("/mark")
-    public ResponseEntity<String> markEntry(@RequestHeader("Authorization") String authtoken, @RequestParam("id") String id, @RequestParam("marked") boolean marked) {
+    public ResponseEntity<String> markEntry(@RequestHeader("Authorization") String authtoken,
+            @RequestParam("id") String id, @RequestParam("marked") boolean marked) {
 
         EntryService service = new EntryService(factory);
 
         try {
             return ResponseEntity.ok(gson.toJson(service.markEntry(authtoken, id, marked).getMessage()));
-        } catch(Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
 
     @GetMapping("/item/history/{name}")
-    public ResponseEntity<String> itemHistory(@RequestHeader("Authorization") String authtoken, @PathVariable("name") String name) {
+    public ResponseEntity<String> itemHistory(@RequestHeader("Authorization") String authtoken,
+            @PathVariable("name") String name) {
 
         ChartDataService service = new ChartDataService(factory);
 
         try {
             return ResponseEntity.ok(gson.toJson(service.getItemHistory(authtoken, name).getData()));
-        } catch(Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.internalServerError().body(e.getMessage());
         }
     }
